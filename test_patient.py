169f5e1975235ef62fd595a8db8167614cdd57e2
Nora-p <nora.npc@hotmail.com>
2022-12-02 04:06:44 +0100

    Refactor code

--- test_patient.py
+++ test_patient.py
@@ -1,7 +1,7 @@
 import Patient_new
 
 def test_name():
-    assert Patient_new.patient.findname("John","Smith") == "John Smith"
+    assert Patient_new.patient.findname() == "John Smith"
 
 def test_age():
     assert Patient_new.patient.findage() == 20
