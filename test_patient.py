24b0e0750b5be92d46c0825606ac61248332ec45
Nora-p <nora.npc@hotmail.com>
2022-12-02 04:25:18 +0100

    Refactor code

--- test_patient.py
+++ test_patient.py
@@ -1,17 +1,17 @@
 import Patient_new
 
 def test_name():
-    assert Patient_new.patient.findname() == "John Smith"
+    assert Patient_new.patient.findname() == ("John", "Smith")
 
 def test_age():
-    assert Patient_new.patient.findage() == 20
+    assert Patient_new.patient.findage(20) == 20
 
 def test_sal():
-    assert Patient_new.patient.findsal() == 44000
+    assert Patient_new.patient.findsal(44000) == 44000
 
 def test_gender():
-    assert Patient_new.patient.findgender() == "male"
+    assert Patient_new.patient.findgender(male) == "male"
 
 def test_phone ():
-    assert Patient_new.patient.findphone() == "0708888888"
+    assert Patient_new.patient.findphone(0708888888) == "0708888888"
 
